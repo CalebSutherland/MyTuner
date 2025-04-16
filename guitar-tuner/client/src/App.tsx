6c76a00197dae6d5e18856c86588231921d975a8
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-04-16 15:33:40 -0700

    Add basic Node.js/Express.js backend

---
+++ guitar-tuner/client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { useEffect } from "react";
+import Tuner from "./components/Tuner"
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    fetch('http://localhost:3001/api/ping')
+      .then(res => res.json())
+      .then(data => console.log(data.message))
+      .catch(err => console.error('Error fetching from backend:', err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>MyTuner</h1>
+      <div>
+      <Tuner />
+      </div>
+    </div>
+  )
+}
+
+export default App
